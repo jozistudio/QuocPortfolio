d906e7908543c3404af5525c0b8ec7eae31b9a60
JoziStudio <quoctuandoan2010@gmail.com>
2018-03-13 16:13:25 +1100

    add quote-machine.js

--- quote-machine/quote-machine.js
+++ quote-machine/quote-machine.js
@@ -1,17 +1,37 @@
 var jsonStr = "";
 var path = "https://jozistudio.github.io/QuocPortfolio/";
 var folderName = "quote-machine";
+var index = 0;
+var counter = 0;
+var quotes = undefined;
+var singleQuote = undefined;
+
 $(document).ready(function() {
+	function updateQuote(q, a){
+		$("#quote").html(q);	
+		$("#author").html(a);	
+	}
 	
+	$.getJSON(path + folderName + "/json/quotes.json",function(json){
+		//jsonStr = JSON.stringify(json);
+		jsonStr = json;
+		quotes = jsonStr.quotes;
+		counter = quotes.length; // get total number of items on JSON object
+		if (counter > 0) {
+			index = Math.floor(Math.random() * counter);
+			console.log(index);
+			updateQuote(quotes[index].quote, quotes[index].author);
+		}		
+		else 
+			alert("Can't parse JSON file.");
+	});
 	
 	
 	$("#getRandomQuote").on("click", function(){
-		console.log("Get Random button is clicked");
-		$("#quote").html("Here is the message");
-		$.getJSON(path + "/json/quotes.json",function(json){
-			jsonStr = JSON.stringify(json);
-			$("#quote").html(jsonStr);
-		});
+		index++;
+		if (index >= counter)
+			index = 0;
+		updateQuote(quotes[index].quote, quotes[index].author);
 	});
 });
 
